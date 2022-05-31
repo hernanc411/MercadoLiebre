7420b630eaad945f28270c167c27b67b72556df3
Hernan Castro <62190091+hernanc411@users.noreply.github.com>
2022-05-31 14:25:10 -0300

    se agrego una ruta

--- app.js
+++ app.js
@@ -9,6 +9,9 @@ app.listen(process.env.PORT || 3000,()=> {
     console.log("Servidor corriendo en el puerto 3000")
 });
 
+app.get('/',function(req,res){
+    res.sendFile(path.resolve(__dirname,'./views/home.html'));
+})
 
 app.get('/home',function(req,res){
     res.sendFile(path.resolve(__dirname,'./views/home.html'));
